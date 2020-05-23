d890cce9113fd51850f0b2acd6e7e04f65001970
Aaron <chi.wang@nottingham.ac.uk>
2020-05-23 18:00:21 +0100

    Accomplish the second Kata's code

--- fundamental/Your-order-please.py
+++ fundamental/Your-order-please.py
@@ -4,14 +4,14 @@
 
 def order(sentence):
     # Transform str to list, split by space
-    l = sentence.split(" ")
-    # Extract the number of each word and make dict
-    pass
-    # Sort the dict by number
+    word_list = sentence.split(" ")
+    # Extract the number of each word and make tuple, then Sort it
+    sorted_list = sorted([(character, word) for word in word_list for character in word if character.isdigit()])
     # Make the new string
-    return string
+    result = " ".join([item[1] for item in sorted_list])
+    return result
 
 
 if __name__ == '__main__':
-    string = order("4of Fo1r pe6ople g3ood th5e the2")
+    string = order("4of Fo3r pe6ople g3ood th5e the2")
     print(string)
