ad3fea1246cdf23806d5a8b8cc15e11121d567dc
Aaron <chi.wang@nottingham.ac.uk>
2020-05-23 12:43:27 +0100

    Add the second Kata's code structure

---
+++ fundamental/Your-order-please.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+
+def order(sentence):
+    # Transform str to list, split by space
+    l = sentence.split(" ")
+    # Extract the number of each word and make dict
+    pass
+    # Sort the dict by number
+    # Make the new string
+    return string
+
+
+if __name__ == '__main__':
+    string = order("4of Fo1r pe6ople g3ood th5e the2")
+    print(string)
